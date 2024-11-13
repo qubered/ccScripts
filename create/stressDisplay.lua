91d36413087edc39339cc01a196678a8701f0b16
Jayden <156985453+qubered@users.noreply.github.com>
2024-11-13 22:10:48 +1100

    made a bit bigger

--- create/stressDisplay.lua
+++ create/stressDisplay.lua
@@ -4,22 +4,22 @@ monitor = peripheral.wrap("front")
 
 local function displayStressInfo()
     monitor.clear()
-    monitor.setTextScale(0.5)
+    monitor.setTextScale(0.8)
     
     monitor.setCursorPos(1, 1)
     monitor.write("Current Stress:")
     monitor.setCursorPos(1, 2)
-    monitor.write("**" .. current .. "su**")
+    monitor.write(current .. "su")
     
     monitor.setCursorPos(1, 3)
     monitor.write("Total Stress:")
     monitor.setCursorPos(1, 4)
-    monitor.write("**" .. total .. "su**")
+    monitor.write(total .. "su")
     
     monitor.setCursorPos(1, 5)
     monitor.write("Stress Percentage:")
     monitor.setCursorPos(1, 6)
-    monitor.write("**" .. string.format("%.2f", stressPercent) .. "%**")
+    monitor.write(string.format("%.2f", stressPercent) .. "%")
 end
 
 while true do
